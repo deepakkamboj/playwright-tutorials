b0b1485c7ad606272b600fa482db8fdf384c1cb4
Deepak Kamboj <deepakkamboj@gmail.com>
2025-08-30 12:09:49 -0700

    Enhance line highlighting and line number options

--- slides/engine.js
+++ slides/engine.js
@@ -5,18 +5,30 @@ module.exports = ({ marp }) =>
 
     marpit.highlighter = function (code, lang, attrs) {
       const html = highlighter.call(this, code, lang, attrs)
+
+      // Match line highlight ranges {5,7-9}
       const m = (attrs || '').toString().match(/{([\d,-]+)}/)
       const ranges = m
         ? m[1].split(',').map(r => r.split('-').map(n => parseInt(n, 10)))
         : []
 
-      const lines = html.split(/\n(?!$)/) // keep trailing newline intact
+      // Detect "no-linenos"
+      const disableLineNumbers = (attrs || '').toString().includes("no-linenos")
+
+      const lines = html.split(/\n(?!$)/)
       const items = lines.map((line, i) => {
         const ln = i + 1
         const on = ranges.some(([a, b]) => (b ? a <= ln && ln <= b : ln === a))
-        return `<li${on ? ' class="hl"' : ''}><span data-lno></span><span>${line}</span></li>`
+
+        if (disableLineNumbers) {
+          // no line number span
+          return `<li${on ? ' class="hl"' : ''}><span>${line}</span></li>`
+        } else {
+          // with line number span
+          return `<li${on ? ' class="hl"' : ''}><span data-lno></span><span>${line}</span></li>`
+        }
       })
 
-      return `<ol>${items.join('')}</ol>`
+      return `<ol class="code-block${disableLineNumbers ? ' no-linenos' : ''}">${items.join('')}</ol>`
     }
   })
